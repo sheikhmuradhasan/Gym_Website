0894b2e0b0f3c5a125aba68c9f4052e28a10d692
Murad Hasan <sheikhmuradhasan@gmail.com>
2025-01-14 20:33:46 +0600

    Update loginAction.php

--- loginAction.php
+++ loginAction.php
@@ -5,7 +5,7 @@ if(isset($_POST['loginForm'])){
     $l_email = $_POST['l_email'];
     $l_password = $_POST['l_password']
 
-    $result = mysqli_query();
+    $result = mysqli_query($conn,"SELECT * FROM `web` WHERE username='$l_username' And Password='$l_pass'");
 
     if(mysqli_num_rows($result)>0){
         $_SESSION['username']=$l_username;
