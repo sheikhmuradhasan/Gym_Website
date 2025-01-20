302c6408d9203cfac578df2fb9f9e3aff64394d2
Murad Hasan <sheikhmuradhasan@gmail.com>
2025-01-21 02:00:39 +0600

    Update deleteCart.php

--- deleteCart.php
+++ deleteCart.php
@@ -12,7 +12,16 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $user_id = $_SESSION['user_id'];
     $product_id = $_SESSION['product_id'];
 
-    $deleteQuery = 
+    $deleteQuery = "DELETE FROM `cart` WHERE user_id = '$user_id' AND product_id = '$product_id'";
+
+    if(mysqli_query($conn, $deleteQuery)){
+        echo "<script>alert('Product deleted successfully from cart!!')</script>;";
+    } else{
+        echo "<script>alert('There is an error. Product couldn't deleted.')</script>;";
+    }
+
+    echo "<script>location.href='cart.php'</script>;";
+
 }
 
 
